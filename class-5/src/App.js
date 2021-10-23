e3ed99c76f6f600dd1279e94b3b504c9874e8025
sergiorodas <sergiorodasbj@hotmail.com>
2021-10-23 18:47:55 -0300

    Fetch the notes from an api, add new States and useEffect

--- class-5/src/App.js
+++ class-5/src/App.js
@@ -1,10 +1,21 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 import { Note } from './Note'
 
-export default function App(props) {
-    const [notes, setNotes ] = useState(props.notes)
+export default function App() {
+    const [notes, setNotes ] = useState([])
     const [newNote, setNewNote] = useState('')
+    const [loading, setLoding] = useState(false)
+
+    useEffect(() => {
+      setLoding(true)
+      setTimeout(() => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(response => response.json())
+        .then(json => setNotes(json))
+        setLoding(false)
+      }, 2000)
+    }, [])
 
     const handleChange = (event) => {
       setNewNote(event.target.value)
@@ -25,10 +36,13 @@ export default function App(props) {
     return (
       <div>
         <h1>Notas</h1>
-        <ul>
+        {
+          loading ? 'Cargando...' : "" 
+        }
+        <ol>
             {notes
             .map(note => <Note key={note.id} {...note}/>)}
-        </ul>
+        </ol>
         <form onSubmit={handleSubmit}>
           <input type="text" onChange={handleChange} value={newNote}/>
           {/* by default the last button of a form works as a submit */}

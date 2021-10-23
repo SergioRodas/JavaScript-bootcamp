78c84f7698607e4be5492d682c7a5ab266b0b96d
sergiorodas <sergiorodasbj@hotmail.com>
2021-10-23 18:20:07 -0300

    Change the properties and content of the notes

--- class-5/src/App.js
+++ class-5/src/App.js
@@ -5,7 +5,6 @@ import { Note } from './Note'
 export default function App(props) {
     const [notes, setNotes ] = useState(props.notes)
     const [newNote, setNewNote] = useState('')
-    const [showAll, setShowAll] = useState(true)
 
     const handleChange = (event) => {
       setNewNote(event.target.value)
@@ -15,30 +14,20 @@ export default function App(props) {
       event.preventDefault()
       const noteToAddToState = {
         id: notes.length + 1,
-        content: newNote,
-        date: new Date().toISOString(),
-        important: Math.random() < 0.5
+        title: newNote,
+        body: newNote,
       }
       setNotes([...notes, noteToAddToState])
       setNewNote('')
     }
 
-    const handleShowAll = () => {
-      setShowAll(() => !showAll)
-    }
 
     return (
       <div>
         <h1>Notas</h1>
-        <button onClick={handleShowAll}>{showAll ? 'Mostrar solo lo importante' : 'Mostrar todo'}</button>
         <ul>
             {notes
-            /* filter should always return a boolean */
-            .filter((note) => {
-              if (showAll === true) return true
-              return note.important === true
-            })
-            .map(note => <Note key={note.id} id={note.id} content={note.content} date={note.date}/>)}
+            .map(note => <Note key={note.id} {...note}/>)}
         </ul>
         <form onSubmit={handleSubmit}>
           <input type="text" onChange={handleChange} value={newNote}/>
---
+++ class-5/src/Note.js
@@ -0,0 +1,10 @@
+export const Note = ({ title, body }) => {
+    return (
+    <li>
+        <p>{title}</p>
+        <small>
+            {body}
+        </small>
+    </li>
+    )
+}
